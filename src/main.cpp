a536f55fa848a9c0781e44e60c59cb9401544778
Austen Cabret <austencabret@gmail.com>
2024-07-31 10:24:20 -0400

    add --version function

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,19 @@ namespace fs = std::filesystem;
 class CubeGenerator
 {
 public:
+    static constexpr const char *VERSION = "CubeGenerator 0.0.1";
+
     CubeGenerator(int argc, char **argv)
     {
+        // Parse command-line arguments
+        for (int i = 1; i < argc; ++i)
+        {
+            if (std::string(argv[i]) == "--version")
+            {
+                std::cout << VERSION << '\n';
+                exit(EXIT_SUCCESS);
+            }
+        }
     }
 
     struct NeuralNetwork : torch::nn::Module
@@ -25,6 +36,10 @@ public:
 
         NeuralNetwork(int inputSize)
         {
+            if (inputSize <= 0)
+            {
+                throw std::invalid_argument("Error: Invalid input size.");
+            }
             hidden1 = register_module("hidden1", torch::nn::Linear(inputSize, 8));
             hidden2 = register_module("hidden2", torch::nn::Linear(8, 8));
             hidden3 = register_module("hidden3", torch::nn::Linear(8, 8));
@@ -53,11 +68,18 @@ public:
 
     int extractNumberFromFilename(const std::string &filename)
     {
-        std::regex regex("\\d+");
-        std::smatch match;
-        if (std::regex_search(filename, match, regex) && !match.empty())
+        try
         {
-            return std::stoi(match.str());
+            std::regex regex("\\d+");
+            std::smatch match;
+            if (std::regex_search(filename, match, regex) && !match.empty())
+            {
+                return std::stoi(match.str());
+            }
+        }
+        catch (const std::exception &e)
+        {
+            std::cerr << "Error extracting number from filename: " << e.what() << std::endl;
         }
         return 0;
     }
@@ -104,25 +126,33 @@ public:
     std::vector<std::pair<std::string, torch::Tensor>> loadOffFilesFromDirectory(const std::string &directory)
     {
         std::vector<std::pair<std::string, torch::Tensor>> vertices;
-        std::vector<std::string> filePaths;
 
-        for (const auto &entry : fs::directory_iterator(directory))
+        try
         {
-            if (entry.is_regular_file() && entry.path().extension() == ".off")
+            std::vector<std::string> filePaths;
+
+            for (const auto &entry : fs::directory_iterator(directory))
             {
-                filePaths.push_back(entry.path().string());
+                if (entry.is_regular_file() && entry.path().extension() == ".off")
+                {
+                    filePaths.push_back(entry.path().string());
+                }
+            }
+
+            std::sort(filePaths.begin(), filePaths.end(), [this](const std::string &a, const std::string &b)
+                      { return extractNumberFromFilename(a) < extractNumberFromFilename(b); });
+
+            for (const std::string &filePath : filePaths)
+            {
+                if (!readOffFile(filePath, vertices))
+                {
+                    std::cerr << "Failed to load file: " << filePath << '\n';
+                }
             }
         }
-
-        std::sort(filePaths.begin(), filePaths.end(), [this](const std::string &a, const std::string &b)
-                  { return extractNumberFromFilename(a) < extractNumberFromFilename(b); });
-
-        for (const std::string &filePath : filePaths)
+        catch (const std::exception &e)
         {
-            if (!readOffFile(filePath, vertices))
-            {
-                std::cerr << "Failed to load file: " << filePath << '\n';
-            }
+            std::cerr << "Exception caught while loading OFF files: " << e.what() << std::endl;
         }
 
         return vertices;
@@ -139,6 +169,7 @@ public:
         }
 
         torch::Tensor result = torch::stack(flattenedTensors);
+
         return result;
     }
 
@@ -164,18 +195,19 @@ public:
     {
         std::ostringstream formatted_array;
         formatted_array << "OFF\n8 6 0\n";
-
         auto flattened = tensor.view({-1});
         size_t numVertices = 8;
+
         for (auto i : std::views::iota(0u, numVertices))
         {
-            const size_t baseIndex = i * 3;
-            formatted_array << flattened[baseIndex].item<float>() << " "
-                            << flattened[baseIndex + 1].item<float>() << " "
-                            << flattened[baseIndex + 2].item<float>() << "\n";
+            const size_t BASEINDEX = i * 3;
+            formatted_array << flattened[BASEINDEX].item<float>() << " "
+                            << flattened[BASEINDEX + 1].item<float>() << " "
+                            << flattened[BASEINDEX + 2].item<float>() << "\n";
         }
 
         std::string vertex_section = formatted_array.str();
+
         if (!vertex_section.empty() && vertex_section.back() == '\n')
         {
             vertex_section.pop_back();
@@ -197,8 +229,8 @@ public:
         std::string filePath = originalFilePath;
         std::string fileName = filePath.substr(0, filePath.find_last_of('.'));
         std::string fileExtension = filePath.substr(filePath.find_last_of('.'));
-
         int fileCounter = 1;
+
         while (std::filesystem::exists(filePath))
         {
             filePath = fileName + "_" + std::to_string(fileCounter++) + fileExtension;
@@ -217,17 +249,17 @@ public:
 
     int run()
     {
-        std::string training_directory = "../assets/datasets/austens_boxes/training";
-        std::string target_directory = "../assets/datasets/austens_boxes/target";
+        std::string TRAINING_DIRECTORY = "../assets/datasets/austens_boxes/training";
+        std::string TARGET_DIRECTORY = "../assets/datasets/austens_boxes/target";
 
-        std::vector<std::pair<std::string, torch::Tensor>> training_vector = loadOffFilesFromDirectory(training_directory);
-        std::vector<std::pair<std::string, torch::Tensor>> target_vector = loadOffFilesFromDirectory(target_directory);
+        std::vector<std::pair<std::string, torch::Tensor>> training_vector = loadOffFilesFromDirectory(TRAINING_DIRECTORY);
+        std::vector<std::pair<std::string, torch::Tensor>> target_vector = loadOffFilesFromDirectory(TARGET_DIRECTORY);
 
         int inputSize = training_vector.size();
         NeuralNetwork model(inputSize);
         torch::manual_seed(1);
-
         const short NOISE = 200;
+
         torch::Tensor trainingTensor = combineTensors(training_vector).transpose(0, 1) * NOISE;
         torch::Tensor targetTensor = combineTensors(target_vector).transpose(0, 1);
 
@@ -236,13 +268,14 @@ public:
         trainModel(model, trainingTensor, TRAINING_TARGET);
 
         torch::Tensor output = model.forward(trainingTensor).detach();
+
         std::string output_formatted = formatToOFF(output);
-        std::string file_path = "../assets/generated_boxes/generated_box.off";
-        saveOffFile(file_path, output_formatted);
+        std::string FILE_PATH = "../assets/generated_boxes/generated_box.off";
+        saveOffFile(FILE_PATH, output_formatted);
 
         std::shared_ptr<NeuralNetwork> net = std::make_shared<NeuralNetwork>(inputSize);
-        std::string model_save_path = "./model.pt";
-        torch::save(net, model_save_path);
+        std::string MODEL_SAVE_PATH = "./model.pt";
+        torch::save(net, MODEL_SAVE_PATH);
 
         return 0;
     }
@@ -254,13 +287,10 @@ int main(int argc, char **argv)
     return app.run();
 }
 
-// todo: check allcaps on variables
+// todo: add app --version function and --help function
 // todo: make separate .cpp and .h files  to improve readability
 // todo: put .cpp in src/ and .h into include/
-// todo: use Doxygen
 // todo: make main.cpp the neural network definition, make a trainer file and make a cube generator file
-// todo: check if i should do more or less error handling
+// todo: use Doxygen
 // todo compare to hazel
 // todo: delete reference/
-// todo: check line breaks
-// todo: add app --version function and --help function
